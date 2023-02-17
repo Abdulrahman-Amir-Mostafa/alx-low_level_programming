2d8164d96d11bb779d817d3006064806ee6e0613
Bodamiru <bodamiru@proton.me>
2023-02-18 01:52:29 +0200

    #41

---
+++ 0x01-variables_if_else_while/9-print_comp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main Bodamiru code
+ * Description: msh faddy ashr7lk
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i,", i);
+	}
+	printf("\n");
+	return (0);
+}
