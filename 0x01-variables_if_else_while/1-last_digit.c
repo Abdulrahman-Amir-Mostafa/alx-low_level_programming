cf1b0e8417453acf2c05d69ca94e68aafa6c0b08
Bodamiru <bodamiru@proton.me>
2023-02-17 22:56:29 +0200

    #7

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 		printf("is %i and is greater than 5\n",n);
-	else if (1 =< n < 6) 
+	else if (0 < n < 6) 
 		printf("is %i and is less than 6 and not 0\n",n);
 	else
 	       	printf("is %i and is 0\n",n);
