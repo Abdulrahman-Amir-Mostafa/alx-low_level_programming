ea17541d1452102734712de73320b441ed270741
Bodamiru <bodamiru@proton.me>
2023-02-18 01:11:21 +0200

    #32

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main Bodamiru code
+ * Description: program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i > 10 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
