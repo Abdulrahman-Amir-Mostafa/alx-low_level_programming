8f22ae9dba7407aa5505234f5afdf59070bec8b7
Bodamiru <bodamiru@proton.me>
2023-02-18 00:00:00 +0200

    #16

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main Bodamiru code
+ * Description: program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
