8e77ed19d00c202d70145d529201d34e231fc10b
Bodamiru <bodamiru@proton.me>
2023-02-18 02:11:56 +0200

    #48

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,18 @@
 
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 0; i < 9; i++)
+	for (n = 0; n <= 9; n++)
 	{
-		printf("%i, ", i);
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
-	for (i = 9)
-	{
-		printf("%i", i);
 
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
+
