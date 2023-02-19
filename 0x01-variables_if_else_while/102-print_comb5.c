6ed5b36a126c18d0d2d846d55ce3ecea117fb583
Abdulrahman Amir Mostafa <110491969+Abdulrahman-Amir-Mostafa@users.noreply.github.com>
2023-02-20 00:02:40 +0200

    END

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,41 +5,25 @@
  * Description: Msh Faddy Ashr7 L7ddrtk
  * Return: Always 0 (Succes)
  */
-
-int main(void)
+ 
+ int main(void)
 {
-	int one;
-	int two;
-	int onnen;
-	int twoeen;
+	int i;
+	int j;
 
-for ( one = 0 ; one < 10 ; one++)
-{	
-
-	for (two = 0 ; two <= 10 ; two++)
+	for (i = 48; i < 57; i++)
 	{
-	
-		for (onnen = 0 ; onnen < 10 ; onnen++)
+		for (j = i + 1; j < 58; j++)
 		{
-			for ( twoeen = 0 ; twoeen < 10 ; twoeen++)
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-				putchar('0' + one);
-				putchar('0' + two);
-				putchar(32);
-				putchar('0' + onnen);
-				putchar('0' + twoeen);
-				if (!(one == 9 && two == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				twoeen++;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
-
-
